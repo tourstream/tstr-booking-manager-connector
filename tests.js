3ae8f56683f93570586be83ea6168c0f4082eb1b
Steve Reichenbach <steve.reichenbach@tourstream.eu>
2017-11-16 15:54:39 +0100

    initial commit

--- tests.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const testsContext = require.context('./tests', true, /\.test\.js$/);
-testsContext.keys().forEach(testsContext);
-
-const srcContext = require.context('./src', true, /\.js$/);
-srcContext.keys().forEach(srcContext);
